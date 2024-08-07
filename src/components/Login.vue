f0562eaa1351824aa13cd335c6179533e0fef519
tsaimogono <156794044+tsaimogono@users.noreply.github.com>
2024-08-07 13:58:46 +0200

    Displays products in a responsive grid layout, with each product shown in a styled card that includes an image, details, and action buttons for viewing details and adding to the cart.

---
+++ src/components/Login.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+      <div v-if="loading" class="text-center text-lg font-bold">Loading...</div>
+      <div v-else>
+        <div class="mb-4">
+          <label for="category" class="mr-2">Filter by Category:</label>
+          <select v-model="selectedCategory" @change="filterProducts" class="p-2 border border-gray-300 rounded">
+            <option value="">All</option>
+            <option v-for="category in categories" :key="category" :value="category">
+              {{ category }}
+            </option>
+          </select>
+    
+          <label for="sorting" class="ml-4 mr-2">Sort by Price:</label>
+          <select v-model="sorting" @change="handleSort" class="p-2 border border-gray-300 rounded">
+            <option value="default">Default</option>
+            <option value="low">Lowest to Highest</option>
+            <option value="high">Highest to Lowest</option>
+          </select>
+    
+          <button @click="resetFilters" class="ml-4 bg-gray-800 text-white py-2 px-4 rounded hover:bg-gray-700">Reset Filters</button>
+        </div>
+    
+  <div class="bg-gray-100 p-6">
+    <div class="container mx-auto">
+      <div class="product-grid grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div
+          v-for="product in filteredProducts"
+          :key="product.id"
+          class="product-card border border-gray-200 p-4 text-center bg-white shadow-lg rounded-lg hover:shadow-xl transition-shadow transform hover:-translate-y-2"
+        >
+          <img
+          img :src="product.image" alt="Product Image" class="w-full h-[15rem] object-contain mb-4" 
+          />
+          <h2 class="text-lg font-semibold mb-2">{{ product.title }}</h2>
+          <p class="text-gray-800 mb-2 font-bold">${{ product.price.toFixed(2) }}</p>
+          <p class="text-gray-500 mb-4 text-sm">{{ product.category }}</p>
+          <p class="mb-4">Rating: {{ product.rating.rate }} ({{ product.rating.count }} reviews)</p>
+          <router-link :to="{ name: 'ProductDetail', params: { id: product.id } }">
+            <button class="bg-blue-600 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+              View Details
+            </button>
+          </router-link>
+          <button
+            @click="addToCart(product)"
+            class="mt-2 mr-2 bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700 transition-colors focus:outline-none focus:ring-3 focus:ring-green-500 focus:ring-opacity-50"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+ </div>
+</div>
+ </template>
+  
